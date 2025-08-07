ff8ed02c4d8cd5583db4dfa0e88052f9d151c091
martins <ozolins.martinsh@gmail.com>
2025-08-07 09:41:53 +0300

    fix: add truncate to content section

--- components/PostInformation.tsx
+++ components/PostInformation.tsx
@@ -79,7 +79,9 @@ export default function PostInformation({
         <h1 className="text-2xl font-bold text-gray-900 mb-4 break-words">
           {title}
         </h1>
-        <p className="text-gray-800 whitespace-pre-line mb-4">{content}</p>
+        <p className="text-gray-800 whitespace-pre-line mb-4 break-words">
+          {content}
+        </p>
         {categories && (
           <p className="text-sm text-gray-500 italic">
             Categories: {categories}
